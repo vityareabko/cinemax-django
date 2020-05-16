c258e15c1318b7a0a6e62f8d0b115211cc9d20c8
vityareabko <reabko15@gmail.com>
2020-05-16 03:14:36 +0300

    сссс

--- cinemax_project1/settings.py
+++ cinemax_project1/settings.py
@@ -71,9 +71,7 @@ ROOT_URLCONF = 'cinemax_project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
