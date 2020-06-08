39e5ae02b475161701b31059b36cad32d45f7cdd
vityareabko <reabko15@gmail.com>
2020-06-08 23:37:32 +0300

    chat v0.1

--- chatroom/views.py
+++ chatroom/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.utils.safestring import mark_safe
+import json
 
 
 def index(request):
     return render(request, 'chatroom/chatroom.html')
 
+@login_required
 def room(request, room_name):
     return render(request, 'chatroom/room.html', {
-        'room_name': room_name
+        'room_name_json': room_name, #room_name
+        'username': request.user.username, #request.user.username
     })
