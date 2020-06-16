ae259bbda0d7e6541a0b5f3f25d07286cd706a41
vityareabko <reabko15@gmail.com>
2020-06-17 01:43:58 +0300

    sendgrid

--- cinemax_project1/settings.py
+++ cinemax_project1/settings.py
@@ -183,11 +183,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = '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'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = False
 
 SITE_ID = 1
 
