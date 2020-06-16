e9bc1434aa6144a9c897145dfe7f15d369c73757
vityareabko <reabko15@gmail.com>
2020-06-17 01:13:43 +0300

    add send grid

--- cinemax_project1/settings.py
+++ cinemax_project1/settings.py
@@ -171,14 +171,23 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'cinemacount12090@gmail.com'
+# EMAIL_HOST_PASSWORD = 'cinemaccountx12'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'cinemacount12090@gmail.com'
-EMAIL_HOST_PASSWORD = 'cinemaccountx12'
+EMAIL_HOST_USER = 'cinemax'
+EMAIL_HOST_PASSWORD = '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'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
 
 
 SITE_ID = 1
