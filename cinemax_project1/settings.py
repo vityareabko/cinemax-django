a37f69a257b08d2fa11a4eee528df12f7c5f02f3
vityareabko <reabko15@gmail.com>
2020-06-17 16:52:08 +0300

    deploy

--- cinemax_project1/settings.py
+++ cinemax_project1/settings.py
@@ -236,7 +236,7 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-AWS_LOCATION = 'media'
+AWS_LOCATION = 'posters'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
