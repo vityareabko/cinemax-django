2388b8faf5508d36fb3e3b11d7e3d87f40cda6e1
vityareabko <reabko15@gmail.com>
2020-06-17 19:06:40 +0300

    add sort by desc id news

--- news/views.py
+++ news/views.py
@@ -89,7 +89,7 @@ class MovieNewsList(ListView): #### парсить дание в когда ча
     # context_object_name = 'posts'
     template_name = 'news_template/news.html'
     paginate_by = 5
-    
+    ordering = ['-id']
     # def get(self, request):
     #     news_list = ParseMovieInfo.objects.order_by('-id').all()
     #     current_page = Paginator(news_list,6)
