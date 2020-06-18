006e0652d4b8596d723cb7bf64d19d24bb0c95d6
vityareabko <reabko15@gmail.com>
2020-06-18 10:03:15 +0300

    deploy

--- cinemax_project1/settings.py
+++ cinemax_project1/settings.py
@@ -234,7 +234,7 @@ AWS_STORAGE_BUCKET_NAME = 'cinemaxpro-bucket'
 # AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_PUBLIC_MEDIA_LOCATION = 'media/tikets'
 
 # AWS_LOCATION = 'media'
